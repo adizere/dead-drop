84625ce64ba0b10d912f468a65c4b1bacf9bcd01
Adi Seredinschi <a@seredinschi.net>
2026-02-07 12:59:21 -0500

    fixed task name

--- v0/plugins/encrypt-and-store/index.js
+++ v0/plugins/encrypt-and-store/index.js
@@ -1,50 +1,61 @@
 import { task } from "hardhat/config";
 import { ArgumentType } from "hardhat/types/arguments";
 
+const TASK_NAME = "encrypt-and-store";
+
+const encryptStoreDescription =
+  "Encrypt a secret and store it via EncryptedCalldataStorage.storeEncrypted";
+
+const encryptStoreOptions = (t) =>
+  t
+    .addOption({
+      name: "id",
+      description: "Human-readable identifier (will be hashed to bytes32 dataId)",
+      type: ArgumentType.STRING,
+      defaultValue: "",
+    })
+    .addOption({
+      name: "message",
+      description: "Plaintext message to encrypt (utf8)",
+      type: ArgumentType.STRING_WITHOUT_DEFAULT,
+      defaultValue: undefined,
+    })
+    .addOption({
+      name: "file",
+      description: "Path to a file whose bytes will be encrypted",
+      type: ArgumentType.FILE_WITHOUT_DEFAULT,
+      defaultValue: undefined,
+    })
+    .addOption({
+      name: "contract",
+      description:
+        "Existing EncryptedCalldataStorage address; if omitted, it will be deployed",
+      type: ArgumentType.STRING_WITHOUT_DEFAULT,
+      defaultValue: undefined,
+    })
+    .addOption({
+      name: "keysOut",
+      description:
+        "Where to write the generated ML-KEM keypair JSON (default: keys/<id>.key.json)",
+      type: ArgumentType.FILE_WITHOUT_DEFAULT,
+      defaultValue: undefined,
+    })
+    .setAction(() => import("./task-action.js"));
+
 /**
- * Custom plugin that registers the `encrypt-and-store` task.
+ * Custom plugin that registers the `encrypt-and-store` and `store:encrypt` tasks.
  *
  * Hardhat 3 tasks are registered via plugins (not by calling task() in a random imported file).
  */
 export default {
   id: "shared-secret:encrypt-and-store",
   tasks: [
-    task(
-      "encrypt-and-store",
-      "Encrypt a secret and store it via EncryptedCalldataStorage.storeEncrypted",
-    )
-      .addOption({
-        name: "id",
-        description: "Human-readable identifier (will be hashed to bytes32 dataId)",
-        type: ArgumentType.STRING,
-        defaultValue: "",
-      })
-      .addOption({
-        name: "message",
-        description: "Plaintext message to encrypt (utf8)",
-        type: ArgumentType.STRING_WITHOUT_DEFAULT,
-        defaultValue: undefined,
-      })
-      .addOption({
-        name: "file",
-        description: "Path to a file whose bytes will be encrypted",
-        type: ArgumentType.FILE_WITHOUT_DEFAULT,
-        defaultValue: undefined,
-      })
-      .addOption({
-        name: "contract",
-        description: "Existing EncryptedCalldataStorage address; if omitted, it will be deployed",
-        type: ArgumentType.STRING_WITHOUT_DEFAULT,
-        defaultValue: undefined,
-      })
-      .addOption({
-        name: "keysOut",
-        description: "Where to write the generated ML-KEM keypair JSON (default: keys/<id>.key.json)",
-        type: ArgumentType.FILE_WITHOUT_DEFAULT,
-        defaultValue: undefined,
-      })
-      .setAction(() => import("./task-action.js"))
-      .build(),
+    encryptStoreOptions(
+      task(TASK_NAME, encryptStoreDescription),
+    ).build(),
+    encryptStoreOptions(
+      task("store:encrypt", encryptStoreDescription),
+    ).build(),
   ],
 };
 
